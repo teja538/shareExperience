fb3a180ee2ac741f25bdc6108b91ab6f5043b7d0
teja538 <saisree538@gmail.com>
2021-11-11 14:36:28 +0530

    feat service

--- src/app/services/serve-experience.service.ts
+++ src/app/services/serve-experience.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:3000/api/experiences'; //url where backend sends data(apis where we get or post data)
 
 @Injectable({
   providedIn: 'root'
